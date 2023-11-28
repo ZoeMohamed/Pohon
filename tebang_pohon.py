fcdfbad0cce8ec1a08f4895b39be4d6f3c660228
ZoeMohamed <77608814+ZoeMohamed@users.noreply.github.com>
2023-11-28 18:16:34 +0700

    committed

--- tebang_pohon.py
+++ tebang_pohon.py
@@ -21,6 +21,17 @@ def TebangPohon(P):
         return []
     else:
         return [Akar(P)] + TebangPohon(Left(P)) + TebangPohon(Right(P))
+        # [a,[b,[],[]],[c,[],[]]]
+        # [a] + TebangPohon([b,[],[]]) + TebangPohon([c,[],[]])
+        # TebangPohon([b,[],[]]) -> [b]
+        # TebangPohon([c,[],[]]) -> [c]
+
+        # [a] + [b] + [c]
+
+        # [a,[b,[1,[],[]],[]],[c,[],[]]]
+        # [a] + TebangPohon([b,[1,[],[]],[]]) + TebangPohon([c,[],[]])
+        # TebangPohon([b,[1,[],[]],[]])
+        # TebangPohon([1,[],[]])
 def FirstElement(L):
     return L[0]
 def Konso(x,L):
@@ -47,6 +58,7 @@ def Flatten(P):
     return Insort(TebangPohon(P))
 print(MakePB("a",MakePB('a',[],[]),[]))
 print(TebangPohon(MakePB("a",MakePB('a',[],[]),[])))
+print(["aa"] + ["bb"] + ["cc"])
 # def Flattenx(P):
 #     result = [Akar(P)]
     
